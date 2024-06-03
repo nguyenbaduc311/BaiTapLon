b5b6203d34414b1aa9f3ea486154577bb7805aa0
hijoouzc <hooij415@gmail.com>
2024-06-03 21:22:05 +0700

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -21,6 +21,7 @@ public:
 	string getCode();
 	int getQuantity();
 	long long getPrice();
+    long long int getTotalValue() const;
 	void setName(string a);
 	void setCode(string b);
 	void setQuantity(int c);
@@ -30,7 +31,157 @@ public:
 
 string Product::getName(){ return name;} // Các hàm lấy và gán
 string Product::getCode(){ return code;}
-int Product::getQuan8.\n";
+int Product::getQuantity(){ return quantity;}
+long long Product::getPrice(){ return price;}
+long long int Product::getTotalValue() const { return price * quantity;}
+void Product::setName(string a){ name = a;}
+void Product::setCode(string b){ code = b;}
+void Product::setQuantity(int c){ quantity = c;}
+void Product::setPrice(long long d){ price = d;}
+void Product::display() const { // hàm hiển thị các thông số của sản phẩm
+	cout << "Product Code: " << code 
+		 << " | Name: " << name 
+		 << " | Quantity: " << quantity 
+		 << " | Price: " << price << endl;
+}
+class Inventory { // tạo đối tượng kho hàng (để quản lí đối tượng sản phẩm)
+private:
+	vector<Product> products; // Tạo 1 mảng có kiểu dữ liệu là "đối tượng <Product> "
+	
+	int findProductIndexByCode(string code) { // Tìm vị trí của sản phẩm trong mảng sản phẩm
+		for (int i = 0; i < products.size(); i++) { // size() là số phần tử đang có trong mảng
+			if (products[i].getCode() == code) {
+				return i;
+			}
+		}
+		return -1; // Không tìm thấy thì trả về -1
+	}
+
+public:
+	void swapProduct(Product& a, Product& b);
+	void addProduct(Product product);
+	void removeProduct(string code);
+	void importProduct(string code, int quantity);
+	void editProduct(string code, string name, long long price);
+	void exportProduct(string code, int quantity);
+	void errorProduct(string code,int quantity);
+	void calculateTotalValue();
+	void displayInventory();
+};
+
+void Inventory::swapProduct(Product& a, Product& b) {
+    Product temp = a;
+    a = b;
+    b = temp;
+}
+void Inventory::addProduct(Product product) { // Hàm thêm sản phẩm mới
+	int index = findProductIndexByCode(product.getCode()); 
+	if (index == -1) { // Nếu ko có mặt hàng này thì mới thêm vào mảng
+		products.push_back(product); // Hàm pushback là thêm phần tử vào mảng
+		cout << "Product added successfully." << endl;
+	} else {
+		cout << "Product with this code already exists." << endl;
+	}
+}
+void Inventory::removeProduct(string code) { // Hàm xóa sản phẩm khỏi kho
+	int index = findProductIndexByCode(code);
+	if (index != -1) { // erase(*địa chỉ cần xóa*) : để xóa sản phẩm
+		products.erase(products.begin() + index); // begin() là lấy vị trí con trỏ đầu tiên của mảng
+		cout << "Product removed successfully." << endl;
+	} else {
+		cout << "Product not found." << endl;
+	}
+}
+void Inventory::importProduct(string code, int quantity) { // Tìm sản phẩm rồi thêm số lượng sản phẩm
+	int index = findProductIndexByCode(code); 
+	if (index != -1) {
+		products[index].setQuantity(products[index].getQuantity() + quantity); // Thêm số lượng
+		cout << "Product imported successfully." << endl;
+	} else {
+		cout << "Product not found." << endl;
+	}
+}
+void Inventory::editProduct(string code, string name, long long price) { // Sửa thông số của sản phâm (tên, giá)
+	int index = findProductIndexByCode(code); 
+	if (index != -1) {
+		products[index].setName(name); // SỬa lại tên
+		products[index].setPrice(price); // Sửa lại giá
+		cout << "Product edited successfully." << endl;
+	} else {
+		cout << "Product not found." << endl;
+	}
+}
+void Inventory::exportProduct(string code, int quantity) { // Xuất hàng 
+	int index = findProductIndexByCode(code);
+	if (index != -1) {
+		if (products[index].getQuantity() >= quantity) { // check xem có đủ lượng hàng để xuất hàng ko
+			products[index].setQuantity(products[index].getQuantity() - quantity); // Giảm số lượng
+			cout << "Product exported successfully." << endl;
+		} else {
+			cout << "Not enough quantity in stock." << endl;
+		}
+	} else {
+		cout << "Product not found." << endl;
+	}
+}
+void Inventory::errorProduct(string code,int quantity){ // Vứt bỏ hàng lỗi ra khỏi kho
+	int index = findProductIndexByCode(code);
+	if (index != -1) {
+		if (products[index].getQuantity() >= quantity) { // check xem có đủ lượng hàng để trừ ko
+			products[index].setQuantity(products[index].getQuantity() - quantity); // Giảm số lượng hàng hỏng
+			cout << "Error Product removed successfully." << endl;
+		} else {
+			cout << "Not enough quantity in stock." << endl;
+		}
+	} else {
+		cout << "Product not found." << endl;
+	}
+}
+void Inventory::calculateTotalValue(){
+    long long int totalValue = 0;
+    for (const auto &product : products) {
+        totalValue += product.getTotalValue();
+    }
+    cout << "Total Current Inventory Value: " << totalValue << endl;
+}
+void Inventory::displayInventory() { // Hàm hiển thị kho hàng
+    if (products.empty()) {
+        cout << "Inventory is empty." << endl;
+    } else {
+        cout << "=== Current Inventory ===" << endl;
+        cout << "-------------------------" << endl;
+        for (const auto &product : products) {
+            product.display();
+        }
+    }
+}
+
+
+void clearInputBuffer() { // Xóa bộ đệm
+	cin.clear(); 
+	cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Xóa tối ưu hơn thôi.
+}
+
+int main() {
+	Inventory inventory; // Tạo đối tượng KHO
+
+	int choice; // lựa chọn 
+	int choice2;
+	do { // Vòng lặp sự kiện
+		cout << "\nInventory Management System\n"; // MENUUUUUUU
+		cout << "1. Add Product\n";
+		cout << "2. Remove Product\n";
+		cout << "3. Import Product\n";
+		cout << "4. Export Product\n";
+		cout << "5. Edit Product\n";
+		cout << "6. Remove Product error\n";
+		cout << "7. Display Inventory\n";
+		cout << "8. Exit\n";
+		cout << "Enter your choice: ";
+		cin >> choice; // Nhập lựa chọn 
+
+		if (cin.fail()) { // Nếu nhập sai kiểu dữ liệu, thì sẽ ko chạy phần dưới nữa, mà tới vòng lặp mới
+			cout << "Invalid input. Please enter a number between 1 and 8.\n";
 			clearInputBuffer();
 			continue;
 		}
